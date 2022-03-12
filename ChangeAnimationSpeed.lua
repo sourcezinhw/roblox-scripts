2ceee205243287dd87fc162dba8839701f71f698
sourcezinhw <101473811+sourcezinhw@users.noreply.github.com>
2022-03-12 16:52:25 -0300

    Change Animation Speed

---
+++ ChangeAnimationSpeed.lua
@@ -0,0 +1,35 @@
+--[[
+
+    Change Animation Speed
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
+
+repeat wait() until game:IsLoaded()
+setclipboard("https://discord.gg/")
+
+--[[
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
+
+--\ Configuration /--
+
+_G.Speed = 7
+
+--\ Configuration /--
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sourcezinhw/roblox-scripts/main/w/ChangeAnimationSpeed.lua'),true))()
+
+--[[
+
+    Change Animation Speed
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
