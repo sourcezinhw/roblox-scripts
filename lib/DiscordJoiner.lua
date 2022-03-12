d24411e1f7b0ef36643b108f528e8d6b274a3969
source <101473811+sourcezinhw@users.noreply.github.com>
2022-03-12 19:34:53 -0300

    Update DiscordJoiner.lua

--- lib/DiscordJoiner.lua
+++ lib/DiscordJoiner.lua
@@ -1,1 +1,29 @@
+--[[
 
+    Discord Joiner
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
+
+repeat wait() until game:IsLoaded()
+setclipboard("https://discord.gg/")
+
+--[[
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sourcezinhw/roblox-scripts/main/w/DiscordJoiner.lua'),true))()
+
+--[[
+
+    Discord Joiner
+
+    This script was made by source;
+    https://discord.gg/
+
+]]
